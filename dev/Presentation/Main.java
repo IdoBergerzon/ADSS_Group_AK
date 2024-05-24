bd6a895c24485818a797ca85fa3613fb2a056f27
IdoBergerzon <idoberg@post.bgu.ac.il>
2024-05-24 09:59:29 +0300

    add brunch,role,roster and shift classes

--- dev/Presentation/Main.java
+++ dev/Presentation/Main.java
@@ -1,4 +1,10 @@
 package Presentation;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your ID: ");
+        int userID = Integer.parseInt(scanner.nextLine());
+    }
 }
--- dev/Presentation/Worker_Main.java
+++ dev/Presentation/Worker_Main.java
@@ -1,4 +1,9 @@
 package Presentation;
 
+import Domain.Worker_Controller;
+
 public class Worker_Main {
+    private Worker_Controller controller;
+
+
 }
