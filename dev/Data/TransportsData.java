f2f1d01fec092599f52774d38de90b4a27288f99
HeziKadosi <hezi33356@gmail.com>
2024-05-29 17:33:40 +0300

    edit classes

--- dev/Data/TransportsData.java
+++ dev/Data/TransportsData.java
@@ -1,7 +1,22 @@
 package Data;
 
+import Domain.Transport;
+
 import java.util.HashMap;
 
 public class TransportsData {
-    private HashMap<Integer, TransportsData> transports;
+    private HashMap<Integer, Transport> transports;
+
+
+    public TransportsData() {
+        this.transports = new HashMap<>();
+
+    }
+    public HashMap<Integer, Transport> getTransports() {
+        return transports;
+    }
+    public void addTransport(Transport transport) {
+        this.transports.put(transport.getTransportID(), transport);
+    }
+
 }
