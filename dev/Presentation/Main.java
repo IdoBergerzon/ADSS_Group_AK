fabb5e8eb7c9051e2bb0c47a15076fd66d5caf1c
HeziKadosi <hezi33356@gmail.com>
2024-06-02 12:44:55 +0300

    edit classes

--- dev/Presentation/Main.java
+++ dev/Presentation/Main.java
@@ -2,17 +2,67 @@ package Presentation;
 
 import Domain.DriverController;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("Main Menu:");
+            System.out.println("1. Add");
+            System.out.println("2. Update");
+            System.out.println("3. Display");
+            System.out.println("4. Execute Transport");
+            System.out.println("5. Reset Data Bases");
+            System.out.println("6. Exit");
+
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                // Add
+                case 1:
+
+                    break;
+
+                //Update
+                case 2:
+
+                    break;
+
+                //Display
+                case 3:
+
+                    break;
+
+                //Execute Transport
+                case 4:
+
+                    break;
+
+                //Reset Data Bases
+                case 5:
+
+                    break;
+
+                //Exit
+                case 6:
+
+                    break;
+            }
+        }
+        while (choice != 6);
+        scanner.close();
+
+
+
 
 
 
 
         DriverController controller = new DriverController();
 
-        // Create some licenses
-        License license1 = new License("B", 3500);
-        License license2 = new License("C", 7500);
 
 
 
