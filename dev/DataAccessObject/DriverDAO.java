b7abfa31c33d7e0696d78095608e10c4240f70c7
HeziKadosi <hezi33356@gmail.com>
2024-07-02 22:19:39 +0300

    edit classes

--- dev/DataAccessObject/DriverDAO.java
+++ dev/DataAccessObject/DriverDAO.java
@@ -14,58 +14,76 @@ public class DriverDAO implements IDAO<Driver>{
 
     @Override
     public void add(Driver driver) throws SQLException {
-        String sql = "INSERT INTO drivers(driverID, driverName, available, licenseMaxWeight) VALUES(?, ?, ?, ?)";
-        try (Connection connection = DriverManager.getConnection(URL);
-             PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setInt(1, driver.getDriverID());
-            pstmt.setString(2, driver.getDriverName());
-            pstmt.setBoolean(3, driver.isAvailable());
-            pstmt.setInt(4, driver.getLicenseMaxWeight());
-            pstmt.executeUpdate();
+        if (!this.getAll().containsKey(driver.getDriverID())) {
+            String sql = "INSERT INTO drivers(driverID, driverName, available, licenseMaxWeight) VALUES(?, ?, ?, ?)";
+            try (Connection connection = DriverManager.getConnection(URL);
+                 PreparedStatement pstmt = connection.prepareStatement(sql)) {
+                pstmt.setInt(1, driver.getDriverID());
+                pstmt.setString(2, driver.getDriverName());
+                pstmt.setBoolean(3, driver.isAvailable());
+                pstmt.setInt(4, driver.getLicenseMaxWeight());
+                pstmt.executeUpdate();
+            }
         }
+        else
+            System.out.println("Driver already exists");
     }
 
     @Override
     public void remove(Driver driver) throws SQLException {
-        String sql = "DELETE FROM drivers WHERE driverID = ?";
-        try (Connection connection = DriverManager.getConnection(URL);
-             PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setInt(1, driver.getDriverID());
-            pstmt.executeUpdate();
+        if (this.getAll().containsKey(driver.getDriverID())) {
+            String sql = "DELETE FROM drivers WHERE driverID = ?";
+            try (Connection connection = DriverManager.getConnection(URL);
+                 PreparedStatement pstmt = connection.prepareStatement(sql)) {
+                pstmt.setInt(1, driver.getDriverID());
+                pstmt.executeUpdate();
+            }
         }
+        else
+            System.out.println("Driver does not exist");
     }
 
     @Override
     public void update(Driver driver) throws SQLException {
-        String sql = "UPDATE drivers SET driverName = ?, available = ?, licenseMaxWeight = ? WHERE driverID = ?";
-        try (Connection connection = DriverManager.getConnection(URL);
-             PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, driver.getDriverName());
-            pstmt.setBoolean(2, driver.isAvailable());
-            pstmt.setInt(3, driver.getLicenseMaxWeight());
-            pstmt.setInt(4, driver.getDriverID());
-            pstmt.executeUpdate();
+        if (this.getAll().containsKey(driver.getDriverID())) {
+            String sql = "UPDATE drivers SET driverName = ?, available = ?, licenseMaxWeight = ? WHERE driverID = ?";
+            try (Connection connection = DriverManager.getConnection(URL);
+                 PreparedStatement pstmt = connection.prepareStatement(sql)) {
+                pstmt.setString(1, driver.getDriverName());
+                pstmt.setBoolean(2, driver.isAvailable());
+                pstmt.setInt(3, driver.getLicenseMaxWeight());
+                pstmt.setInt(4, driver.getDriverID());
+                pstmt.executeUpdate();
+            }
         }
+        else
+            System.out.println("Driver does not exist");
     }
 
     @Override
     public Driver get(int id) throws SQLException {
-        String sql = "SELECT * FROM drivers WHERE driverID = ?";
-        Driver driver = null;
-        try (Connection connection = DriverManager.getConnection(URL);
-             PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setInt(1, id);
-            ResultSet rs = pstmt.executeQuery();
-            if (rs.next()) {
-                int driverID = rs.getInt("driverID");
-                String driverName = rs.getString("driverName");
-                boolean available = rs.getBoolean("available");
-                int licenseMaxWeight = rs.getInt("licenseMaxWeight");
-                driver = new Driver(driverID, driverName, licenseMaxWeight);
-                driver.setAvailable(available);
+        if (this.getAll().containsKey(id)) {
+            String sql = "SELECT * FROM drivers WHERE driverID = ?";
+            Driver driver = null;
+            try (Connection connection = DriverManager.getConnection(URL);
+                 PreparedStatement pstmt = connection.prepareStatement(sql)) {
+                pstmt.setInt(1, id);
+                ResultSet rs = pstmt.executeQuery();
+                if (rs.next()) {
+                    int driverID = rs.getInt("driverID");
+                    String driverName = rs.getString("driverName");
+                    boolean available = rs.getBoolean("available");
+                    int licenseMaxWeight = rs.getInt("licenseMaxWeight");
+                    driver = new Driver(driverID, driverName, licenseMaxWeight);
+                    driver.setAvailable(available);
+                }
             }
+            return driver;
         }
-        return driver;
+        else {
+            System.out.println("Driver does not exist");
+            return null;
+        }
     }
 
     @Override
--- dev/DataAccessObject/TruckDAO.java
+++ dev/DataAccessObject/TruckDAO.java
@@ -14,72 +14,88 @@ public class TruckDAO implements IDAO<Truck> {
 
     @Override
     public void add(Truck truck) throws SQLException {
-        String sql = "INSERT INTO trucks(truckID, truckType, truckWeight, maxWeight, available) VALUES(?, ?, ?, ?, true)";
-        try (Connection connection = DriverManager.getConnection(URL);
-             PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setInt(1, truck.getTruckID());
-            pstmt.setString(2, truck.getTruckType());
-            pstmt.setDouble(3, truck.getTruckWeight());
-            pstmt.setDouble(4, truck.getMaxWeight());
-            pstmt.setBoolean(5, truck.isAvailable());
-            pstmt.executeUpdate();
+        if (!this.getAll().containsKey(truck.getTruckID())) {
+            String sql = "INSERT INTO trucks(truckID, truckType, truckWeight, maxWeight, available) VALUES(?, ?, ?, ?, true)";
+            try (Connection connection = DriverManager.getConnection(URL);
+                 PreparedStatement pstmt = connection.prepareStatement(sql)) {
+                pstmt.setInt(1, truck.getTruckID());
+                pstmt.setString(2, truck.getTruckType());
+                pstmt.setDouble(3, truck.getTruckWeight());
+                pstmt.setDouble(4, truck.getMaxWeight());
+                pstmt.setBoolean(5, truck.isAvailable());
+                pstmt.executeUpdate();
+            }
         }
+        else
+            System.out.println("Truck Already Exists");
     }
 
     @Override
     public void remove(Truck truck) throws SQLException {
-        String sql = "DELETE FROM trucks WHERE truckID = ?";
-        try (Connection connection = DriverManager.getConnection(URL);
-             PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setInt(1, truck.getTruckID());
-            pstmt.executeUpdate();
+        if (this.getAll().containsKey(truck.getTruckID())) {
+            String sql = "DELETE FROM trucks WHERE truckID = ?";
+            try (Connection connection = DriverManager.getConnection(URL);
+                 PreparedStatement pstmt = connection.prepareStatement(sql)) {
+                pstmt.setInt(1, truck.getTruckID());
+                pstmt.executeUpdate();
+            }
         }
+        else
+            System.out.println("Truck Not Exists");
     }
 
     @Override
     public void update(Truck truck) throws SQLException {
-        String sql = "UPDATE trucks SET truckType = ?, truckWeight = ?, maxWeight = ?, available = ? WHERE truckID = ?";
-        try (Connection connection = DriverManager.getConnection(URL);
-             PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, truck.getTruckType());
-            pstmt.setDouble(2, truck.getTruckWeight());
-            pstmt.setDouble(3, truck.getMaxWeight());
-            pstmt.setBoolean(4, truck.isAvailable());
-            pstmt.setInt(5, truck.getTruckID());
-            pstmt.executeUpdate();
+        if (this.getAll().containsKey(truck.getTruckID())) {
+            String sql = "UPDATE trucks SET truckType = ?, truckWeight = ?, maxWeight = ?, available = ? WHERE truckID = ?";
+            try (Connection connection = DriverManager.getConnection(URL);
+                 PreparedStatement pstmt = connection.prepareStatement(sql)) {
+                pstmt.setString(1, truck.getTruckType());
+                pstmt.setDouble(2, truck.getTruckWeight());
+                pstmt.setDouble(3, truck.getMaxWeight());
+                pstmt.setBoolean(4, truck.isAvailable());
+                pstmt.setInt(5, truck.getTruckID());
+                pstmt.executeUpdate();
+            }
         }
+        else
+            System.out.println("Truck Not Exists");
     }
 
     @Override
     public Truck get(int id) throws SQLException {
-        String sql = "SELECT * FROM trucks WHERE truckID = ?";
-        Truck truck = null;
-        try (Connection connection = DriverManager.getConnection(URL);
-             PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setInt(1, id);
-            ResultSet rs = pstmt.executeQuery();
-            if (rs.next()) {
-                int truckID = rs.getInt("truckID");
-                String truckType = rs.getString("truckType");
-                double truckWeight = rs.getDouble("truckWeight");
-                double maxWeight = rs.getDouble("maxWeight");
-                boolean available = rs.getBoolean("available");
-                truck = new Truck(truckID, truckType, truckWeight, maxWeight);
-                truck.setAvailable(available);
+        if (this.getAll().containsKey(id)) {
+            String sql = "SELECT * FROM trucks WHERE truckID = ?";
+            Truck truck = null;
+            try (Connection connection = DriverManager.getConnection(URL);
+                 PreparedStatement pstmt = connection.prepareStatement(sql)) {
+                pstmt.setInt(1, id);
+                ResultSet rs = pstmt.executeQuery();
+                if (rs.next()) {
+                    int truckID = rs.getInt("truckID");
+                    String truckType = rs.getString("truckType");
+                    double truckWeight = rs.getDouble("truckWeight");
+                    double maxWeight = rs.getDouble("maxWeight");
+                    boolean available = rs.getBoolean("available");
+                    truck = new Truck(truckID, truckType, truckWeight, maxWeight);
+                    truck.setAvailable(available);
+                }
             }
+            return truck;
         }
-        return truck;
+        else {
+            System.out.println("Truck Not Exists");
+            return null;
+        }
     }
 
     @Override
     public HashMap<Integer, Truck> getAll() throws SQLException {
         String sql = "SELECT * FROM trucks";
         HashMap<Integer, Truck> trucksMap = new HashMap<>();
-
         try (Connection connection = DriverManager.getConnection(URL);
              Statement stmt = connection.createStatement();
              ResultSet rs = stmt.executeQuery(sql)) {
-
             while (rs.next()) {
                 int truckID = rs.getInt("truckID");
                 String truckType = rs.getString("truckType");
